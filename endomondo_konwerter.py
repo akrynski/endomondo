41ad2057708f61350120df31b40e6d571b80f161
akrynski <akrynski@wp.pl>
2021-02-07 15:29:27 +0100

    Potrafię elegancko wydobyć wszystkie dane z podstawowych tagów. Zostało dopracować wydobycie z location.

--- endomondo_konwerter.py
+++ endomondo_konwerter.py
@@ -10,6 +10,7 @@ line = None
 aktualny_workout = None
 workout_data = None
 klucze = []
+wartosci = []
 
 os.chdir("C:\\Users\\Andrzej\\Downloads\\endomondo-2021-01-27\\Workouts\\")
 os.system("dir /B /A:-D > .\\output.txt")
@@ -30,10 +31,22 @@ def czytaj_workout(_line):
         workout_data = json.load(aktualny_workout)
         pola = len(workout_data)
         for i in range(pola):
+            '''
+            print("Klucz " + str(i) +" : ", *workout_data[i])
+            kl = [*workout_data[i]]
+            print(kl[0])
+            '''
+            '''
             keyclass = workout_data[i].keys()
             klucz = list(keyclass)[0]
             if klucz not in klucze:
                 klucze.append(klucz)
+            '''
+            klucz = [*workout_data[i]][0]
+            if klucz not in klucze:
+                klucze.append(klucz)
+            wartosci.append({klucz: list(workout_data[i].values())})
+
 
 
     except(IOError, OSError):
@@ -68,16 +81,31 @@ except (IOError, OSError):
     sys.exit(1)
 finally:
     f.close()
-    print("klucze: ", klucze)
-    print()
+    #print("klucze: ", klucze)
+    #print()
+
     for i in range(17):
         if list(workout_data[i].keys())[0] != 'points':
+            '''
             print(list(workout_data[i].keys())[0])
             print(list(workout_data[i].values())[0])
+            '''
+            '''!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'''
+            klucz = klucze[i]
+            print(klucz)
+            print(wartosci[i][klucz][0])
+            '''!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'''
         else:
             location_dic = workout_data[i]['points']
             print(location_dic[0][0])
             print(location_dic[0][1])#dystans do pierwszego pktu
-            print(list(location_dic[0][0])[0])#0=napis klucza location
+            latitude_dic = list((location_dic[0][0]).values())[0][0][0]
+            longitude_dic = list((location_dic[0][0]).values())[0][0][1]
+            print("latitude: ", latitude_dic['latitude'])
+            print("longitude: ", longitude_dic['longitude'])
+
+            #print(list(location_dic[0][0])[0])#0=napis klucza location
             print(location_dic[1][0])
             print(location_dic[0][1])  # dystans do drugiego pktu
+
+            #print(*workout_data[1]) napis klucza source
