42157221c5986f0fb9b50da5f53489caf7cbe0be
akrynski <akrynski@wp.pl>
2021-02-15 11:20:31 +0100

    ...

--- endomondo_konwerter.py
+++ endomondo_konwerter.py
@@ -4,16 +4,13 @@
 # licence: MIT
 # date: 05-02-2021
 #github access key: 044cb299a8c2d590a7d100d092b469ef0e2f0d3b
-#delete it if u makake repo public !!!!!!!!!!!!!
+#delete it if u makes repo public !!!!!!!!!!!!!
 import codecs, os, sys, datetime, json
 
-
 klucze = []
-wartosci = []
 
 os.chdir("C:\\Users\\Andrzej\\Downloads\\endomondo-2021-01-27\\Workouts\\")
 os.system("dir /B /A:-D > .\\output.txt")
-
 
 def czytaj_wiersz(plik):
     for wiersz in plik:
@@ -29,14 +26,13 @@ def czytaj_wiersz(plik):
 def czytaj_workout(_line):
     try:
         print("Otwieram plik " + _line.strip())
-        if _line.strip() == "output.txt":
+        if _line == "output.txt":
             return 0
-        _aktualny_workout = codecs.open(_line.strip(), 'r', 'utf-8', 'ignore')
+        _aktualny_workout = codecs.open(_line, 'r', 'utf-8', 'ignore')
         print("Json ładuje plik " + _line)
         workout_data = json.load(_aktualny_workout)
         pola = len(workout_data)
-        klucze = []
-        wartosci = []
+        klucze = []# to nie deklaracja a czyszczenie - deque?
         '''for i in range(pola):
             klucz = [*workout_data[i]][0]
             if klucz not in klucze:
@@ -52,7 +48,7 @@ def czytaj_workout(_line):
 
         for i, v in enumerate(klucze):
             #print("i=",i, "v=",v)
-            if v != 'points':
+            if  v != 'points':
                 print('index = ',i)
                 print('klucz = ',list(workout_data[i].keys())[0])
                 print('wartość: ',workout_data[i][v])
@@ -77,18 +73,22 @@ def czytaj_workout(_line):
                     for i,v in enumerate(pictures_dic_list):
                         for item in v:
                             #print(item)
-                            if 'picture'in item:
+                            if 'picture' in item:
                                 print('url: ', item['picture'][0][0]['url'])
-
                     print(10*'-')
+                elif v == 'tags':
+                    print("SEKCJA TAGS")
+                    tagi = workout_data[i][v]
+                    print('tagi:', tagi[0][0]['name'])
+                    print(10 * '-')
             elif v == 'points':
-                print('v==points: ',v)
                 print(i)
                 print('TU SĄ POINSY')
+                #print('v==points: ',v)
                 print(10*'v')
-                print('dla points index = ',i)#ale tu o 1 więcej - 15,16 itp
+                #print('dla points index = ',i)#ale tu o 1 więcej - 15,16 itp
                 location_dic_list = workout_data[i]['points']
-                print("Jeślinie widać lokacji, to odkomentuj dyrektywę CONTINUE w linii 76\n")
+                print("Jeśli nie widać lokacji, to zakomentuj dyrektywę CONTINUE w linii 92\n")
                 continue
                 for i,v in enumerate(location_dic_list):
                     print(i,'||||||||||||||||||||||||||||||\n')
@@ -99,7 +99,8 @@ def czytaj_workout(_line):
                         if 'distance_km' in item:
                             print('przebyty dystans: ', item['distance_km'], 'km')
                         if 'speed_kmh' in item:
-                            print('Prędkość na odcinku: ', item['speed_kmh'],'km/h')
+                            #print('Prędkość na odcinku: ', item['speed_kmh'],'km/h')
+                            print(' '.join(["Prędkość na odcinku: ", str(item['speed_kmh']), 'km/h']))
                 print(10*'*')
             else:
                 continue
